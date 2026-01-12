2a547f5e7cec3bf78827eee150de53d7023b2c43
제갈도원 <insopa01@naver.com>
2026-01-13 02:24:06 +0900

    연동오류

--- JGDW_OOP_ConsoleGame/260107/GameObjects/Item.cs
+++
@@ -1,9 +0,0 @@
-
-public abstract class Item : GameObject
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-
-    public abstract void Use();
-}
